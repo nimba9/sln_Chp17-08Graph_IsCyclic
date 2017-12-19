64ade3627097a7b23985ea33c2acbecf596b89b2
vancisin peter <vancisin.peter@gmail.com>
2017-12-19 22:03:15 +0100

    exercise based on Nakov & Co book & solutions

--- nakovChp17-08Graph_IsCyclic/Program.cs
+++ nakovChp17-08Graph_IsCyclic/Program.cs
@@ -1,15 +1,80 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace nakovChp17_08Graph_IsCyclic
+namespace IsCyclic
 {
-    class Program
+    public class Graph
     {
-        static void Main(string[] args)
+        private int[][] childNodes;
+        public Graph(int[][] nodes)
         {
+            this.childNodes = nodes;
+        }
+
+        public int[][] ChildNodes
+        {
+            get
+            {
+                return this.childNodes;
+            }
+
+            set
+            {
+                this.childNodes = value;
+            }
+        }
+
+        public bool IsCyclic()
+        {
+            Queue<int> nodes = new Queue<int>();
+            bool[] visited = new bool[ChildNodes.GetLength(0)];
+            nodes.Enqueue(0);
+            while (nodes.Count > 0)
+            {
+                int currentNode = nodes.Dequeue();
+                if (visited[currentNode] == true)
+                {
+                    return true;
+                }
+
+                visited[currentNode] = true;
+                foreach (int childNode in this.childNodes[currentNode])
+                {
+                    nodes.Enqueue(childNode);
+                }
+            }
+
+            return false;
+        }
+    }
+
+    public class IsCyclic
+    {
+        public static Graph CreateGraph()
+        {
+            int vertexCount = int.Parse(Console.ReadLine());
+            int[][] graphArray = new int[vertexCount][];
+            for (int i = 0; i < vertexCount; i++)
+            {
+                string sucessors = Console.ReadLine();
+                string[] currentVertexSucessors = sucessors.Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                graphArray[i] = currentVertexSucessors.Select(item => int.Parse(item)).ToArray();
+                Array.Sort(graphArray[i]);
+                if (graphArray[i].Contains(i))
+                {
+                    throw new ApplicationException("A vertex cannot be it's own successor");
+                }
+            }
+
+            Graph newGraph = new Graph(graphArray);
+            return newGraph;
+        }
+
+        public static void Main(string[] args)
+        {
+            Graph myGraph = CreateGraph();
+            Console.WriteLine(myGraph.IsCyclic());
         }
     }
 }
